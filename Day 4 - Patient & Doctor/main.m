ab488a6bbca6b3591cff3a7fce71fe51668dd292
brokeandfurious <forumbomb@gmail.com>
2017-11-04 20:45:06 -0400

    Initial Commit

---
+++ Day 4 - Patient & Doctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Day 4 - Patient & Doctor
+//
+//  Created by Murat Ekrem Kolcalar on 11/4/17.
+//  Copyright © 2017 mumu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
